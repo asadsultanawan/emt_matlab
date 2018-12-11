80ba8016137c834c1a9817ee04d34c6a33af912e
Asad Sultan <38244020+asadsultanawan@users.noreply.github.com>
2018-12-11 18:29:37 +0500

    Update Dipole_modfied.m

--- Dipole_modfied.m
+++ Dipole_modfied.m
@@ -1,10 +1,10 @@
 %Dipole.m
 %Start
 %A simple interpretation
-[X,Y]=meshgrid(-2:0.2:2,-3:0.2:3);%Defining a mesh grid
+[X,Y]=meshgrid(-10:.5:10,-10:.5:10);%Defining a mesh grid
 V = Y./(X.^2+Y.^2); %Electric field line equation
-contour(X,Y,V,100);
+contour(X,Y,V,50,'linewidth',1.5);
 [Ex,Ey]=gradient(V);
 hold on
-quiver(X,Y,-Ex,-Ey,'autoscalefactor',2);  
+quiver(X,Y,-Ex,-Ey,'linewidth',1.5,'autoscalefactor',2);  
 %end
